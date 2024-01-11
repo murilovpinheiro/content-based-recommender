24fa0b88c88862c56e3df2e87acc1eb06642c581
Murilo <murilovpinheiro@gmail.com>
2024-01-11 14:28:54 -0300

    first commit

---
+++ ContentRecommender.py
@@ -0,0 +1,5 @@
+class ContentRecommender:
+    # Para simplificar, vou considerar o recebimento to do o dataset já tratado
+    # Sendo assim, recebe basicamente um dataset binário com as colunas sendo características e linhas como os items
+    def __init__(self):
+        print("EMPTY")
